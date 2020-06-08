68c9e964ada9886bf20a8cf5f73d47779c7b3908
Nate Lovell <Natelovell99@gmail.com>
2020-06-08 11:14:21 -0600

    Create Smart Text Scale.jsx

---
+++ Smart Text Scale.jsx
@@ -0,0 +1,89 @@
+﻿var logoBinary = "\u0089PNG\r\n\x1A\n\x00\x00\x00\rIHDR\x00\x00\x00\u00C8\x00\x00\x00\x1E\b\x06\x00\x00\x00i?Q\u00AC\x00\x00\x00\tpHYs\x00\x00\x00\x01\x00\x00\x00\x01\x00O%\u00C4\u00D6\x00\x00\x00$zTXtCreator\x00\x00\b\u0099sL\u00C9OJUpL+I-RpMKKM.)\x06\x00Az\x06\u00CEjz\x15\u00C5\x00\x00\x0B\u00EBIDATx\u009C\u00ED\u009Cy\u00D0\u0097U\x15\u00C7?\x07\x15Tx\x01Aq_\u00C0\u00A5\u00D4\x12\u00CBT\"\u00C7\r\u00CD1\u0097\u009AlJ\u00CCv\u00B5\u00D4\u00AC\u00D1\u00D2\u0098\u00D4\u0089\u0099F\u00B4ESb\u009C,53MKMpij\u00C44\u0097\f\x053D\x1237\u00C0\x05\x10\u0095]^x\u00BF\u00FDq\u009F\u0087\u00DFy\u00EE\u00EF\u00F9\u00BD\u00CF\u00C2\x0Fu\u009A\u00DFw\u00E67\u00EF\u00BD\u00F7\u009C\u00BB\u009E\u00BB\u009C{\u00EEy^\u00E8\u00A0\u0083\x0E:\u00E8\u00A0\u0083\x0E:\u00E8\u00A0\u00CD\u00B0v\x17(\u00C9\x00\u00CCL\u0092\u00CC\u00CC\u0094Gkw\u00BD\u00FF\x0F\u0090\u00B4\x110\x04X\u00F4^\x19#I\x00\u00E6\u00E5)i\x1F`\u00FF\x12\u00D9\u00FB\x01\u00B7\u009A\u00D9\u0082\r\u00DA\u00C8\r\u0088\u008D\u00EBd\u0092t!pr\u0094\u00DC\x05\u00AC\x056\x01\u00FAJ\u00EA\x01\u00BA$M\x00n\x02\u00A6\x01\x03\u0081\t\u00C0\u0085Qy\x0F\x03{%\u00D1\u0093\u00CD\u00ECnG\u009B\u009C\u00D0V\x00\u00CB\u0081\u00AB\u00CC\u00ECzG\u00DF\x11\u0098\u009EF\t\u008B>\u00FD\u00DB\u00D4t`\x12p\\\u00C5.\x7F\u00C5\u00CCf\u00BB:7\x01\x1E\u00AA\u0090\u00BF\x0F\u00A1\u00EF\u00BE\x1D\u0096\x137`w\u00E0mI\u00F3\u0081s\u00CCl\u008A/H\u00D2/\u0081O&\u00D1\u00B9\u00841\u00AF\u0082\u0093\u00CC\u00EC\u00BFQ\u0099?\x00N\u008A\u00DA\u00D5\u009B<W\x02?*Y\u00DF\u009B\u00C0\u00EF\u0092z6\x03\u00E6\x10\u00E4\u00B8\x02X\x06\u009CafO9\u00FA4\u0097\u00F743\u00FBG^\u00A1\u00C9f{\x0F\u00B0c\x1E9\u00F9\u009B\u00F6e\u008A\u0099\u009DW\u00B2\u00BD\x19\u00D4Z \u00C0V\u00C0\x1E%yG\x11\x06tp\x12?]\u00D2%f\u00B6\x1C@\u00D2^\u00C0h\u00C7?\"\rH\u00DA\x148\b\x18\u00EA\u00E8\u00CF\x00\u00D7\u00BB\u00F8F\u00C0\u00B0\nm?\b8\u00A0\x02?\u00C0\x16Q\u00BCO\u008D2\u00CAb5a\u00E7\x1D\x01\u009C\x03\u00AC[ \u00926\x07>\x0B\fJ\u0092\u00AE\x05\u00C6U,\x7F@N\u00DA\u00D6T\u0093\u00E7}\x15\u00EA{\u00D3\u0085G\u00D2<\u00A1G\x02O\x01\u0098\u00D9JI#\u0080\u00FE\tmi\u00ABB\u0093\u0093lG\u00CA\u00B5{d\u00F9\u00E6f\u00D1\u00A7n\u00C6\n\u00D8\x1F\u00E8\u00EB\u00E2C\u0081/\u00B9\u00F8\u00D9\x11\u00FF..\u00BC'\u00D9\u00C5\x01p\u00BC\u00A4A\u00D4G\u009D\u00C1\u00EA_\u00CC\u00D26<\f\u00BC\u0091\u0084\x0F\u0096\u00B4\u00B7\u00A3\u009DHcqL'\u00BB\u00DB\u0096\u00C5\u00FA\u00F6\u00E5#Q\u00FC\t\u00E0\u009A\u00E4wm\u00F2\u00D7/\n\u00AF*\x1E\u0092S\u00DE\x17\u00A3\u00F8r\x17\u00DE\u00A6\u00A0-\x1B\\\r\u00AD{\u0082x\u00CC\x06f\u00B9\u00F82\u00C2\u008EtL\x12\u00EF\u009BS\u00CF\u00D9\u0092\u00AEJ\u00C2'F\u00B4\u009D\\x\u00BF\u009C\u00FA\u0086\x10\u0084to\u008B\u00F6\u009CO\u00D8y\u00FC1{\bpB\x12_M\x10\u00A09\u009E.\u00C2I\x04\u00B0&\u00E9\u0083W\u0081\u00BA\u00A3:Dv\x12\x00l\u009A\u00FCR\u00F8:,\u00A9#\u00DD\u0090\u00C6\x01\u00AF\x00=4\u00ABZK\u0081\u008F\x01\u00E7&i\u00A7\x03\u00DFL\u00C2\u00A7:\u00DEII\u00BB\u00E2v\u00F8\u00BEt\u00D3\u0098pi[\u00D6\u00D0;\u00BC<\u0095\u00E4\x1F\x06\x1C\u009B\u00A4\u00F5u\u00E5\x03\u00DCef\u00B1\u00CA<\u009A\u0086\u00C6\u00E0\u00FB\u00F7\u00E1\u009C\u00FA\u008E\u008A\u00E2\u00CBhh\x04ci-\u00E7\u00B8\u00EC\u00F3\u0081y4\u00C6\u00D4\u00AB\u008B\u00CBz)\u00A3\u00FD\u0090\u00F4s5pQ\x0E}\u008C\u00B2\u0098\u00A0f|Z\u00D2\x07s\u00D2\u00A7\u00B9r\u00CE\u00CD\u00A1K\u00D28\u00C7\u00B3KDkZT\u0092\u00CEs\u00F4;r\u00E8\x0F:\u00FA_j\u008E\u00C9xW\u00C6\u00DB9\u00F4\u00E7\x1D=>\x15c\u00DE\u009D$u'\u00BCoJ\x1A i\u0098\u00A4\u00B5I\u00DA\x02\x05\u00F53/\u00EFtW\u00CF\u00ED%\u00DB~\u00A5\u00CB3!\u0087\x1E\u00CB\u00F3\u00A7.\u00FC\u00E3\x1C\u00FE\u00C5\u008E\u00FE\t\u0097~\u008F\u00F2\u00B1\u009F\u00E3\u0099\u00ED\u00D2\u00AF.h\u00F7\x1C\u00C7[E\u00CD.\u008Dv\u009C [\u00D5\u00CCw&pCN\u00BA\u00D7Q\u008Fp\u00E1\u00B9\u008Ev\x14pI\u008Br/\u0090\u00B4\u0092\u00EC\u00F1\u00FB\u00FE\x16\u00E5\u00A7h\u00F7Q]T^\u00AF\u00AA\u00AD\u0099\u00BD$\u00E9\u008F4\u00EE\x1Bc\t\u0097\u00E54\u00DF\u00AF\u00CClU\u0089v\u00D4\u00B1RnY\u0082\u00C7k\f[\u00E7\u00D0\u009B\x16\u00AF\u00A4\u00BE\u0084\u00FB_\u008AX\u009E3R\u00D6\x12\u00F5\u00E7a\u00A3b\u0096\u00EAh\u00C7\x02\u00A9\u00DB\u00B0\u0083h\u00E8\u00DA\x1ECa\u009Dy\u00D1\x1F\u00C9W\x02\u00E3\t\x17\u00D8\u00D1\u0092\u00FA\u0099Y\u00D3N\r|\u00AA\u00A0\u00DE\u00BC\x05\u00D2n\x14M\u00CCm\u0080\u0085\x05<\u0097\x13\x16\b\u00C0\x19\x04\u00AB\x0F\x04\x15\u00E9\x17\u00F5\u009BV\u00882\u00F2\\\u00ED\u00C2\u00DB\x16\u00F0\u00A6\x13~o\u00B2\x0B\u00E72\u00E0gI\u00F8p\u0082u\x13\u00EA?=lKP[\u00DB\u008Av\\\u00D2\x0F\u0095tx\u0094V\u00A4\u00E7B\u00D0eO\u00E8\u0085\u00BE\x03\u00D9\u00DD\u00EC\x05\u00E0Y\u0097\u00B7\u00AEeb\u00A8\u00829\u00F1\u00DDD\u009E%)\x033{\u0084\u00C6%|_\x1A\u0096\u00BE\u00C9f6wC5\f8L\u00D2aQZo\u00F2,Z )\u00F6\u008D\u00E2\u00FEn\u00B1\u009F\u00A4t.\u00D6=A6\u0088!\u00A5\x1D\x0Bd\x04p\u00AF\u00A4\u00A9\u0092F%i3\u0080\x1B\tv\u00EA\u0099T\u00EB\u00B4)\u00D8\u00B8\u00E3\x01\u009D\x07<\u00ED\u00E2\u00F1\u00E5.\u00C59\u00C0g\u0080?\u00B8\u00B4\x1E\u0082Yv\x14p$\x10\u009F<\u00EDx0\u00AD\u00D2\u00C7\u00CC.-\u00A9_\u008BE{EN\u00DA\u00A4J\u00AD\u00AA\u008E\u00E1\u00C0_[\u00C8s*\u00F0$Y\u00EBR\u00DE\u00C4<\x1D\u00F86a\x11\u00ACL\u00D2\u00BC<W\x11\u00D4\u00B4\u00B7\u0092\u00F8`\x1Af\u00F3*\u00B2\u00F0c\u009E\u0099\u00CB\u0092\u00FA*\u0098\u00C5\u00D7\x0Bu\x17\u00C8\u00E3d\u00CDq\x00c\u0080G\x14.\u00C1\u00BB\u009A\u00D9\u00C9f\u00F6q3\x1BIc\u0090\u00A0\u00DC\u00C3\u00D6\x00\u009A\x17\u00C8|\u00B2\x0B\u00A4\u00D5c\u00DFcfv\x1B0\u00D9\u00A5\u00F5\x01\u00BA\u00CDl\u009A\u0099M5\u00B3\u009E(O;\u00EE \u00B15\u00AA7\u00DC%\u00E9\u00E5\u00E4\u00B7\u00900>_\u00CB\u00E1\u00BB\u0085\u00B01\u00A4x\x12\u00F8[\u00856\u0095\u00ED\u00D7\f\x1A*\\\u008AT\u009Ew\u00D2\u0090\u00E7\u0091f\u00B6\x0F\u00C1\u00FC\u009E\u00E2\u00C5\u00B803\u00FB\u008D\u0099M4\u00B3#\u00CC\u00EC\u00FE$\u00D9\u00CBs^\u00E2)0\u00C7\u00A5\u00E5\u00C9\u00B3h\u00B1x\u00FA\x1DnL\x17\x10\x16\u00E1\u00D7\x0B\u00F2\x17\u00A2\u00D6\x021\u00B3_\x13\u00DE+.\u00A2\u00D9\u00CCx\x1C\u00F0OI7KJ/\u00C7\u00BE#\u00CF\x00\u008B\u00A2<K\\\u00B8\u00C7\u00CC\u0096\u0092\x1D\u00D0\x1E\u00C2\x02\u00F9\u00B7K\u00DB39ib\u00A4j\u00D9\u00CC(\u00BD7\u00D7\u0088v\u00BB\u00DC\x14\u00957\u0088\u00A0\u009AlKh\u00AF\u00D1\u00BC\u00E1`fk\u00C8.\u0088\u00FB\u00CC\u00DA\u00EE\x1D\u0084\u0099]C\u0090\u00E7\x04\u009A\u00E5y,\ry\u00BE/\u00CD\u00E2\u00B3\x17\u0095\u009F\u00C8\u00C9\u00AB\u00C4\u00E9\u00A2\u00F7\x1B\u009E\x7F\u00EFI1*'\u00AD\x15\x06\u00D2\x18\u00D3\u00AD\u0092v\u00C5\u008B\u00BE2j\u00ABXf\u00B6\u00C8\u00CC. \f\u00EC\u00F7\u0081\u00D7<\x19\u00F8\x1C0C\u00D2Y\u0080W\x1F6%\u00AB\u00FE\x00\u00FC\u00DD\u0085\u00FB\u00E6\u00A8X\x0B\u00CDl5\u00D9\x1Dg\x00aP\x07\u0093EjB}\u009A\u00EC\u00A4+\u00E3;\u00F4n\u00A2I\x1D\u00904\u0080\u00EC\u00CEz\u008A\nL\u00C4uaf\x0B\u00CD\u00EC|`g\u0082<\u00BD\u00FFT*\u00CF\u00C7s\u00E4Y\x06\u00C3i<pB\u00FE\x02\u00D9?1\u00CC\u00F8So}-S\u00EB\u00ADb\u00D5\u00F5\u00C5J]\x13\u00FA\x02\u00CB\u00CD\u00ECbIW\x00_%<p\u00A5\u008F}\u009B\x03\x13\u0081\u00EB\\\u00F6.\u0082y\u00F7\u008C$\u00FE<\u00CD\u00C2\x18\u0082s9\x01\u00DEJ.qs\u00C8>\x00\x1D\b\u00DC\x155o\b\u0080\u0099\u00AD\u00954\x0388I?\u0098\x1C$\u00E5\u00EE\u0094G\u00AB\u0088\u00E1\x15x\u00C7\x03\u008B\u00C9\u00F6\u00E5\u0091\x1C\u00BE\u00B1d}\u00B86'\u00A8\rMo\x15\x00\u00926&\x187*\u00A1\u0085</\x07N\x01\u00BEK\u00B3<\x7F[\u00A2\u00CC\u008D\t\u009BR\u00BF\u00A8\x0F\u00D0p!\u00F1\x0Bd;\u009A\u00E5P\u00E5\u00B8\u00FC!a\x1E\u00F9\x07\u00E0:\u009E\x06\u00EB\x0FI\x17\u00BA\x07\u009A\u00D9\x11m\x13I\u00E7D\x0FA7\u00B8\u00F0JI&\u00E9?I\u00FC6IS\x1C}\u008D\u00A4\u0091Q\u00FEG\u0093\u00DD\x05Is]\u00FA\u00A5\u0092\u00B6\u008Cx/qm\u00F9ID\u00DB\u00AEE\x7F^v<u\x1F\note\x14=\x14\x1EY\u00B2\u00CC\u00C7\u00D4\u008Cy\n\u00CE\u0092y\u00FC(<,\u00A6\u0098\u009C\u00C7\u0097\u0093\u00EF\x02\u0097'O\u009E\u00DF\u0089\u00DAp\u00AB\x0B\u00DF\u009FS\u00DEXIK\x12\u00FA\u0095\u0092\u00CE\u008A\u00F2_\u0094\u00F0\u00ED\x15\u00A5\x1F-\u00E9)\x17\u009F\x13\u0097\x1D\u00D5\u00F3\u00B4\u00E3=\u00A67\u00DE\u00BA\u00A8\u00ABb\u00F9cp\u00B0\u00DC]\u00C0\u00CC\u00BA\u0081[#~\x7F\u00D4m\u009A\u00D4\u009B>\x12\u00DEMx\x04K\u00F16\u00CD\u00A6\u00C3\u00BB\t\u00A6\u00C014,\x1F\x10,R\u00AF\u0093\u00B5\x7F\x0Fq\u00E1G\u00A3rb\u00F3e\u008A\u00D7Z\u00A4W\u0081/\u00A3\u00E8r\u00BC{Qa\n\u00AF\u00CB\u00B1\u00DF\x13\u00C0\u00F6\x04+]\x13\u0092\u00FBI\u0095v\u00E4a\u008B\x1Cy\u00DE\x12\u00F1\x14\u00A9.\u00DB\x114\u0085\x14\u00BBD\u00F4\x07\x14,d\u00BBFm\x1CS\u00AD\u00A9\u0099\x13\u00A6pL\u00EB\u00A0\x1Df\u00DE\u00B7s\u00BE]\u0088/z\u00B1)\u00D0\u0080;\u0093\u00F0\u00839e\u00C6\u009D\x1DOp\u00CE\u009BJ\u00F62\u00F7\x01\u0082\u00B0\u00BC\u009D\u00DE/\u00B6x\u0081\u00C4\u00EF5)\u00F2\x1E,\u00AB\u00C2\u0097\u0091\u00A7\x1A\u00F8\u00B42\u00E3~\u009A\x0B\u00CF\u00A41^\x10L\u00A8e\u00DAQ\x16\u00BEm\u00ABr\u00E4\u00F9V\x14\u00EFn\x11n\u0085Xu\u00FA3A\u00A5\u00BC#\u00AA\u00FB\u00A3%\u00CAj\u0085\r\u00F2\u0092\u00BE\u00A1\u00BCy\u0097\u00925\u00E7\u00C6\u00F5\b\u00F8\x17a\u00A0\u00E6G\u00B45\x14{q\u00A60z\u00BF?\u00BC\b\u00BC\u00EA\u00E2\u0087\u00B6\u00E0\u00CBUY*\u00A2\u00E8>Wz7\u0097\u00D4E\u00B8\x7F\u00A4\u00B8\f\u00F82\u008D\u00EF^\x0E\u0094\u00D4j2\u00D5\u00E9KQ\u00DB\u0096\x12,\u0089)\u00FCd\u00CC[ qy;\u0097lG,\u00CB2\u00EE4\x1B\x14\u00EDX M\u00A6\u00B4d\x07\u00F2\u00BBN\u00BC\u00BA\u00CD\u00CC\u00D6\u009A\u00D9\u00D1f\x16\u00E7_F\u00F9\x01\u0085f\u008F\u00DFu\x13$Q9\u00FC\u008B\u00ED\bIe|\u008D\u00EA ~[\u00E9\rK\n\u00E8'\u00D1PQ\u00E6\x037\u0099\u00D9b\u00B2\x1F)}\u00ABE\u00DE:j\u0095\u00DF\u00C5\u00F3\u00E4\u00D9CV\u009E\u00BE\u008E<_\u00BC\u00F8\x04-k\x04\u00D9\u0086\u00ACo\u00D7\u00E2\u0092\u00F9\u00A0xLk\u00A1\x1D\u00BEX[H\u00DA\u0083\u00AC\u00F5\x00\u00C2@\u00A7\u00F7\u0081Xg\u00EDM\u0088\u00AF\u0093\x1D\u00D0)\u0084\u00D3f\u0085\u00CB\u00E7'\u00CA\u00F6d}|bu\u00EEN\u00E0\u00F3.\u00FE!\u00C2\x0B\u00FF\u00BB\u0089\"\x1D\u00DE\u00ABW\x13\x13\x137\u00C0\u00ED\u00C0s\x04\x0B\u00DF\t\u0092v0\u00B3yM\u00B9\x1B(k\x05\u00F2\u00F2H\u00E5\x19\u0097\u00B3\u0082\u00C6\u0087c\u00DE}|gI\u0097\u00915\u00A9\u00FB\u00F7\u008B\u00CD\u00C8N\u00FA\u00CB\t*\u00F8\n\u00C2\x06\u00BD\x1B\u00C1Z\x06a>\u00FA\u00BB\u00CB\x1AI#\t\u00E3\u00D5?\u00F9\u00BB\x1B\u00C1\x17\u00CC\u00C8\u009A\u00F8\u00DFm\u00F7\u00A1\x06$\u008DS5Lt\u00E1\u00D5\u008A\x1E\u00F8$\u00FD\u00C9\u00D1gJz\u00D1\u00C5\u008F\u008Bx\u0091\u00F4\u0084\u00A3_!\u00E9I\x17\u009F\x1C\u00F1\u00F7\u0097\u00B4\u00CC\u00D13\u00DF.$<\u00EF\u0084\u00BB\u00FBs\u008E\u00BEJ\u00D2R\x05K\u00CF\u0092$\u009C\u00FE\u00BC\u00AB\u00F8\x12I\u0083\u00A3r\u00CEt\u00F4\u008Bs\u00EA\u00A9\u00E3\u00EE^U\u009E_\u00A8\u00C0{\u00AF\x0Bw+\u00A8\u008F\u00BE\u00EEa\u0092z*\u00D6\u009F\u0087Uj\u008C\u00E5\u0092(\u00FCBEq\u00AEC]\x15+\u00BE7\x14\u00C1\u00EF.\u00AF\x16\u00FCC\u0082Y\u0084S\x01\u0082\u00DA\u0092\u00B9\u00C4'j\u0093\u00B7o\x0F#\u00BB\x03\u00BE\x12\u00F1/'{\u00C1\u00DD\u00A7\\\u0093+C-\u00C2)\u00BC\u00C9\u00B2\x1F\u00E1\u00A1\u00B3+\u00F9\rp?\u00FFy\u00EF\u00D5f\x16\x1B<\u00AE#\u009C\u00B2\x00\u00A7\u00AA=\u008E\u0097/W\u00E4\x7F\u00A9\x02\u00AF\u00D7\x06f$^\x12\x1E\x0B\boa\u00EB\u008B~4\u00C6\u00B2+\n\x17:\u0087\u00B6B]\x15k\x16\u00D9\u00C7\u00BD<t\x13.\u00EA]d'Go*\x01\u0084ct:A\x1F]\u00953A ,\u009A\u00A3\t\u00E6\u00C4\u00A1d\r\x02y\u009E\u00AE\u00BF\u00A7\u00F1\u00E5\u00E2\u00E8\x1Cz\u00BB}\u00B1\u00F2T\u009B\"\u00F7\u00F6\x14\u00DD$\u00DFh\u0093\u00E3\u00AChf\u00CB%M\"|\u00B6\u00BC\u0088`\u00C9{\u00ACB;\u00F3\u0090'\u00CF\u00F8\x1FK\u00A4\u00F2\x1CH\u00B8<\x7F\u008F\u00E0C\u00E6\u00F9|x\bA\u0086\x03i|e\u00FADN\x7F\u00904\x158\u009E\u00F2\u00C6\u0099w\f\u00EDw\u00EC\u00C9\u0081\u0082JuXR\u00DFkf6+\u00A2\u008F$\u00D8\u00C0\u008D\u00E0l\u00F8@\u00C9r\u00FB\x11\u00EE\x1F\u00FD\u0093\u00BF+\u00807\u00E2\u00EFD\x14>\u00D69 )_f\u00F6PD?\u0085 \u00C4.`\u00A9\u0099\u00DD\\\u00A3\u008Fc\b\u00C6\u0085\x1E`\u00A8\u0099]\x1A\u00D1\u0087\u0093}\u00A3\u0089\u00F1j\u00D2\x07\u00E2\u00FF:R\u00B1\x1D\u00DF \u0098z\x07\x01\u008B\u00CD,~\u0093z\u00CF\"Q\u00BF\u0086\u00D3\x1E\u00AB\"\u00847\u00A1~\x04\u00A3\u00D0\u00B3E\u00CC\x1Dt\u00D0A\x07\x1Dt\u00D0A\x07\x1D\u00BC#\u00F8\x1Fb\u009B\u00E0\u009A\u00C2$->\x00\x00\x00\x00IEND\u00AEB`\u0082";
+
+// Script thanks to Nasko 
+// https://www.youtube.com/channel/UCxGJYMOkFhPfLM_Yy6P45lg
+
+var mainWindow = new Window("palette", "", undefined);
+mainWindow.orientation = "column";
+
+var groupOne = mainWindow.add("group", undefined, "groupOne");
+groupOne.orientation = "row";
+var logo = groupOne.add("image", undefined, logoBinary);
+
+var groupTwo = mainWindow.add("group", undefined, "groupTwo ");
+groupTwo .orientation = "row";
+var widthText = groupTwo.add("statictext", undefined, "Width");
+widthText.size = [36, 16];
+var widthEditText = groupTwo.add("edittext", undefined, "1920");
+widthEditText.characters = 6;
+
+var groupThree = mainWindow.add("group", undefined, "groupThree ");
+groupThree .orientation = "row";
+var heightText = groupThree.add("statictext", undefined, "Height");
+heightText.size = [36, 16];
+var heightEditText = groupThree.add("edittext", undefined, "1080");
+heightEditText.characters = 6;
+
+var groupFour = mainWindow.add("group", undefined, "groupFour");
+groupFour.orientation = "row";
+var expressionCheckbox = groupFour.add("checkbox", undefined, "Apply Controls");
+expressionCheckbox.value = true;
+var applyButton = groupFour.add("button", undefined, "Apply!");
+
+mainWindow.center();
+mainWindow.show();
+
+applyButton.onClick = function() {
+        if(app.project.activeItem == null || !(app.project.activeItem instanceof CompItem)) {
+                alert("Please select a composition and at least one text layer", "");
+                return false;
+            }
+        if(app.project.activeItem.selectedLayers.length < 1) {
+            alert("Please select at least 1 text layer to apply to");
+            return false;
+            }
+        var checkedTextLayers = checkForTextLayers(app.project.activeItem.selectedLayers);
+        if(checkedTextLayers.length == 0) {
+                alert("Please select at least 1 text layer to apply to");
+                return false;
+            }
+        
+        app.beginUndoGroup("Auto Text Layer Scaling");
+        applyExpressions(checkedTextLayers, parseFloat(widthEditText.text), parseFloat(heightEditText.text), expressionCheckbox.value);
+        app.endUndoGroup();
+    }
+
+function applyExpressions(layers, width, height, expressionBool) {
+    var comp = layers[0].containingComp;
+        
+    var widthEffect, heightEffect;
+    for(var i = 0; i < layers.length; i++) {
+        if(expressionBool) {
+            if(layers[i].property("ADBE Transform Group").property("ADBE Scale").expressionEnabled == false) {
+                widthEffect = layers[i].Effects.addProperty("ADBE Slider Control");
+                widthEffect.name = "Width";
+                widthEffect.property(1).setValue(width);
+                heightEffect = layers[i].Effects.addProperty("ADBE Slider Control");
+                heightEffect.name = "Height";
+                heightEffect.property(1).setValue(height);
+                layers[i].property("ADBE Transform Group").property("ADBE Scale").expression = 'maxW = effect("Width")("Slider");\rmaxH = effect("Height")("Slider");\rr = sourceRectAtTime(time);\rw = r.width;\rh = r.height;\rs = w/h > maxW/maxH ? maxW/w : maxH/h;\r[100, 100]*s';
+                }
+            } else {
+                if(layers[i].property("ADBE Transform Group").property("ADBE Scale").expressionEnabled == false) {
+                    layers[i].property("ADBE Transform Group").property("ADBE Scale").expression = 'maxW = ' + width + ';\rmaxH = ' + height +';\rr = sourceRectAtTime(time);\rw = r.width;\rh = r.height;\rs = w/h > maxW/maxH ? maxW/w : maxH/h;\r[100, 100]*s';
+                    }
+                }
+        }
+    }
+
+function checkForTextLayers(layers) {
+        var foundLayers = [];
+        
+            for(var i = 0; i < layers.length; i++) {
+                if(layers[i].property("Source Text")) {
+                    foundLayers.push(layers[i]);
+                    }
+                }
+        
+        return foundLayers;
+    }
